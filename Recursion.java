1800996455dce3ac445c18c7b1ff352d0fb608f2
Michael <mlin01@stuy.edu>
2019-02-03 19:58:46 -0500

    make all sums finished

--- Recursion.java
+++ Recursion.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Recursion{
   /*
   *@param n any non-negative value you want to take the sqrt of
@@ -10,7 +12,7 @@ public class Recursion{
      return sqrtH(n,1,tolerance);
   }
 
-  public static double sqrtH(double n, double guess, double tolerance){
+  private static double sqrtH(double n, double guess, double tolerance){
      if(Math.abs((n-(guess*guess)) / n)  <= tolerance) {
        return guess;
     }
@@ -21,26 +23,40 @@ public class Recursion{
     return fibH(n,0,1);
   }
   
-  public static int fibH(int n, int first, int second){
+  private static int fibH(int n, int first, int second){
     if(n == 0) return first;
     return fibH(n-1,second,first+second);
   }
   
   public static ArrayList<Integer> makeAllSums(int n){
     ArrayList<Integer> sums = new ArrayList<>();
-    if(n==0) sums.add(0);
-    else{
-      sums = 
-    }
-    
+    return makeAllSumsH(n,sums);
   }
- 
+
+  private static ArrayList<Integer> makeAllSumsH(int n, ArrayList<Integer> sums){
+    if(n==0) {
+      sums.add(0);
+      return sums;
+    }
+    else{
+      int size = sums.size();
+      for(int i = 0; i<size;i++){
+        //System.out.println("sums(i): "+sums.get(i));
+        sums.add(sums.get(i)+n);
+      }
+      sums.add(n);
+      return makeAllSumsH(n-1,sums);
+    }
+  }
   
   
  public static void main(String args[]){
    //System.out.println(sqrt(100,0.00001));
+   /*
    for(int i = 0; i < 15; i++){
     System.out.println(fib(i));
    }
+   */
+  System.out.println(makeAllSums(4));
  }
 }
